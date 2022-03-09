76c0f31848bb5d4400a9be90bc8076bf4d919d0f
ink <ink@gmail.com>
2022-03-09 21:25:58 +0800

    second commit

---
+++ src/main/java/com/ink/Test.java
@@ -0,0 +1,19 @@
+package com.ink;
+
+import sun.applet.Main;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ink
+ * @Date: 2022/03/09/21:17
+ * @Description:
+ */
+public class Test {
+    private int age;
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("test 222");
+    }
+}
